a21a6964eb89e39e7d5eb5cb67d59c18386335fb
Beniamin Księżarek <48290159+Zamasu97@users.noreply.github.com>
2019-03-06 16:57:47 +0100

    Add files via upload

---
+++ lab1/Untitled.py
@@ -0,0 +1,65 @@
+
+# coding: utf-8
+
+# In[22]:
+
+
+x=[56]
+y=[]
+for i in range(100-56):
+    x.append(x[i]+1)
+for j in range(len(x)):
+    y.append(2*x[j]**2 + 2* x[j] + 2)
+
+
+# In[31]:
+
+
+print('Insert your value here:')
+n = int(input())
+silnia=1
+for i in range(1,n+1):
+    silnia=i*silnia
+print('The factorial of the inserted value is', silnia)
+
+
+# In[49]:
+
+
+def z3(x):
+    xmin=x[0]
+    index=0
+    for i in range (1,len(x)):
+        if (x[i]<xmin):
+            xmin=x[i]
+            index=i
+    print('The lowest value in the provided array is', xmin, 'located on the', index, 'index')
+
+
+# In[51]:
+
+
+z3([0, -10, 54, 6])
+
+
+# In[76]:
+
+
+from numpy import *
+from numpy.random import *
+import matplotlib.pyplot as plt
+
+print('Insert desired length of the chart:')
+n = int(input())/2
+x=linspace(-10,10,200)
+y=x**2
+plt.plot(x,y)
+plt.xlim(-n,n)
+plt.show()
+
+
+# In[70]:
+
+
+
+
