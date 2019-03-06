0e103bffa139ac891c224e5f137ccc552315ccb5
Beniamin Księżarek <48290159+Zamasu97@users.noreply.github.com>
2019-03-06 22:25:51 +0100

    Update zad2.py

--- lab1/zad2.py
+++ lab1/zad2.py
@@ -1,6 +1,9 @@
 print('Insert your value here:')
 n = int(input())
-silnia=1
-for i in range(1,n+1):
-    silnia=i*silnia
-print('The factorial of the inserted value is', silnia)
+if (n<0):
+    print('n<0, cant calculate the factorial!')
+else:
+    silnia=1
+    for i in range(2,n+1):
+        silnia=i*silnia
+    print('The factorial of the inserted value is', silnia)
