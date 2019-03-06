6fccf28b0618e76b3649d0c36cfb5cbb23b9572b
Beniamin Księżarek <48290159+Zamasu97@users.noreply.github.com>
2019-03-06 22:22:33 +0100

    Update zad3.py

--- lab1/zad3.py
+++ lab1/zad3.py
@@ -1,10 +1,14 @@
 def z3(x):
     xmin=x[0]
     index=0
+    ind=[]
     for i in range (1,len(x)):
         if (x[i]<xmin):
             xmin=x[i]
             index=i
-    print('The lowest value in the provided array is', xmin, 'located on the', index, 'index')
+    for i in range (len(x)):
+        if (x[i]==xmin):
+            ind.append(i)
+    print('The lowest value in the provided array is', xmin, 'located on the', ind, 'index/indexes')
 
 z3([0, -10, 54, 6])
