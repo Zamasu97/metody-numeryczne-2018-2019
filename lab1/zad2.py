86041bb2f46b2af6e7e640e98f26c090eee844e9
Beniamin Księżarek <48290159+Zamasu97@users.noreply.github.com>
2019-03-07 18:07:10 +0100

    Update zad2.py

--- lab1/zad2.py
+++ lab1/zad2.py
@@ -1,9 +1,18 @@
+#zad 2
+import sys
+check=True
 print('Insert your value here:')
-n = int(input())
-if (n<0):
-    print('n<0, cant calculate the factorial!')
-else:
-    silnia=1
-    for i in range(2,n+1):
-        silnia=i*silnia
-    print('The factorial of the inserted value is', silnia)
+try:
+    n = int(input())
+except ValueError:
+    print("Provided number is not integrer, cant calculate the factorial!")
+    check=False
+while (check==True):
+    if (n<0):
+        print('n<0, cant calculate the factorial!')
+    else:
+        silnia=1
+        for i in range(2,n+1):
+            silnia=i*silnia
+        print('The factorial of the inserted value is', silnia)
+    check=False
