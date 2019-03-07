e91a28c4604e6e79411d55b2eced3919e5d7309f
Beniamin Księżarek <48290159+Zamasu97@users.noreply.github.com>
2019-03-07 18:23:58 +0100

    Update zad4.py

--- lab1/zad4.py
+++ lab1/zad4.py
@@ -3,14 +3,14 @@ from numpy.random import *
 import matplotlib.pyplot as plt
 
 print('Insert desired length of the chart:')
-n = int(input())/2
+n = int(input())
+stop=n+1
 if(n<=0):
     print('length must be >0!')
 else:
-    start=-10
-    stop=10
+    start=0
     x=linspace(start,stop,200)
     y=x**2
     plt.plot(x,y)
-    plt.xlim(-n,n)
+    plt.xlim(start,n)
     plt.show()
